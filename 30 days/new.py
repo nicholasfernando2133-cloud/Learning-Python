ac13a242610f5ebd10fe7bf82de3fae326f4416d
Nicholas Fernando <283613053+nicholasfernando2133-cloud@users.noreply.github.com>
2026-06-04 19:23:44 +0700

    Delete 30 days/new.py

--- 30 days/new.py
+++
@@ -1,8 +0,0 @@
-from countries_data import countries_data
-
-sorted_by_population = sorted(countries_data, key=lambda x: x['population'], reverse=True)
-
-print('10 most populated countries:')
-for i in range(10):
-    country = sorted_by_population[i]
-    print(f"{i+1}. {country['name']}: {country['population']}")
